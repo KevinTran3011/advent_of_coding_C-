014c421c1407c386131004fd73c195287c8ad69f
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-03-21 15:49:46 +1100

    added code for day 3

---
+++ day_3/part_1/checkGearTotal.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <set>
+#include <cmath>
+#include <map>
+using namespace std;
+
+bool isSymbol(int i, int j, const vector<string> &lines, int n, int m)
+{
+    if (!(0 <= i && i < n && 0 <= j && j < m))
+    {
+        return false;
+    }
+    return lines[i][j] != '.' && !isdigit(lines[i][j]);
+}
+
+int checkGearTotal(const string &inputFileName)
+{
+    ifstream file(inputFileName);
+    string line;
+    vector<string> lines;
+    while (getline(file, line))
+    {
+        lines.push_back(line);
+    }
+
+    int n = lines.size();
+    int m = lines[0].size();
+
+    int total = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        int j = 0;
+        while (j < m)
+        {
+            int start = j;
+            string num = "";
+            while (j < m && isdigit(lines[i][j]))
+            {
+                num += lines[i][j];
+                ++j;
+            }
+            if (num.empty())
+            {
+                ++j;
+                continue;
+            }
+            int numInt = stoi(num);
+            if (isSymbol(i, start - 1, lines, n, m) || isSymbol(i, j, lines, n, m))
+            {
+                total += numInt;
+                continue;
+            }
+            for (int k = start - 1; k <= j + 1; ++k)
+            {
+                if (isSymbol(i - 1, k, lines, n, m) || isSymbol(i + 1, k, lines, n, m))
+                {
+                    total += numInt;
+                    break;
+                }
+            }
+        }
+    }
+    return total;
+}
+
+int main()
+{
+    cout << checkGearTotal("input.txt") << endl;
+    return 0;
+}
---
+++ day_3/part_2/gearTotal.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+bool isSymbol(int i, int j, int num, const vector<string> &lines, vector<vector<vector<int>>> &gears, int n, int m)
+{
+    if (!(0 <= i && i < n && 0 <= j && j < m))
+    {
+        return false;
+    }
+    if (lines[i][j] == '*')
+    {
+        gears[i][j].push_back(num);
+    }
+    return lines[i][j] != '.' && !isdigit(lines[i][j]);
+}
+
+int checkParttNum(const string &input)
+{
+    ifstream file(input);
+    string line;
+    vector<string> lines;
+    int finalResult = 0;
+    int gearTotal = 0;
+    while (getline(file, line))
+    {
+        lines.push_back(line);
+    }
+
+    int n = lines.size();
+    int m = lines[0].size();
+    vector<vector<vector<int>>> gears(n, vector<vector<int>>(m));
+
+    for (int i = 0; i < n; ++i)
+    {
+        int j = 0;
+        while (j < m)
+        {
+            int start = j;
+            string numStr = "";
+            while (j < m && isdigit(lines[i][j]))
+            {
+                numStr += lines[i][j];
+                ++j;
+            }
+            if (numStr.empty())
+            {
+                ++j;
+                continue;
+            }
+            int num = stoi(numStr);
+            isSymbol(i, start - 1, num, lines, gears, n, m) || isSymbol(i, j, num, lines, gears, n, m);
+            for (int k = start - 1; k < j + 1; ++k)
+            {
+                isSymbol(i - 1, k, num, lines, gears, n, m) || isSymbol(i + 1, k, num, lines, gears, n, m);
+            }
+        }
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            if (lines[i][j] == '*' && gears[i][j].size() == 2)
+            {
+                gearTotal = gears[i][j][0] * gears[i][j][1];
+                cout << gears[i][j][0] << "|" << gears[i][j][1] << "|" << gearTotal << endl;
+
+                finalResult += gearTotal;
+            }
+        }
+    }
+    return finalResult;
+}
+
+int main()
+{
+    cout << checkParttNum("input.txt") << endl;
+    return 0;
+}
